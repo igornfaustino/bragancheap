503dbfada8feac96b1eea1b2488a6245b6e6cff5
igornfaustino <igornfaustino@gmail.com>
2020-06-09 11:37:16 +0100

    [screen] add home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,91 +1,103 @@
-import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import React, { useState, useMemo, useCallback } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import { Searchbar } from 'react-native-paper';
 
-import { MonoText } from '../components/StyledText';
+export default function HomeScreen({ navigation }) {
+  navigation.setOptions({ headerShown: false });
 
-export default function HomeScreen() {
+  const [isShowingMoreProducts, setIsShowingMoreProducts] = useState(false);
+  const [isShowingMoreServices, setIsShowingMoreServices] = useState(false);
+
+  const produtos = useMemo(() => {
+    const produtosMostrados = [];
+    if (!isShowingMoreProducts) {
+      for (let i = 0; i < 3; i++) {
+        produtosMostrados.push(
+          <Image
+            style={styles.image}
+            source={{ uri: 'https://dummyimage.com/360x360/777/aaa' }}
+          />
+        );
+      }
+    } else {
+      for (let i = 0; i < 7; i++) {
+        produtosMostrados.push(
+          <Image
+            style={styles.image}
+            source={{ uri: 'https://dummyimage.com/360x360/777/aaa' }}
+          />
+        );
+      }
+    }
+    return produtosMostrados;
+  }, [isShowingMoreProducts]);
+
+  const servicos = useMemo(() => {
+    const servicosMostrados = [];
+    if (!isShowingMoreServices) {
+      for (let i = 0; i < 3; i++) {
+        servicosMostrados.push(
+          <Image
+            style={styles.image}
+            source={{ uri: 'https://dummyimage.com/360x360/777/aaa' }}
+          />
+        );
+      }
+    } else {
+      for (let i = 0; i < 10; i++) {
+        servicosMostrados.push(
+          <Image
+            style={styles.image}
+            source={{ uri: 'https://dummyimage.com/360x360/777/aaa' }}
+          />
+        );
+      }
+    }
+    return servicosMostrados;
+  }, [isShowingMoreServices]);
+
+  const produtosMessage = useMemo(
+    () => (isShowingMoreProducts ? 'Mostrar menos' : 'Mostrar Mais'),
+    [isShowingMoreProducts]
+  );
+
+  const servicesMessage = useMemo(
+    () => (isShowingMoreServices ? 'Mostrar menos' : 'Mostrar Mais'),
+    [isShowingMoreServices]
+  );
+
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
+      <Searchbar placeholder="Pesquisar" />
+      <ScrollView style={styles.content}>
+        <View>
+          <Text>Produto</Text>
+          <View style={styles.flex}>{produtos}</View>
+          <TouchableOpacity
+            onPress={() => setIsShowingMoreProducts((prev) => !prev)}
+          >
+            <Text style={styles.mostrarMais}>{produtosMessage}</Text>
+          </TouchableOpacity>
         </View>
 
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Open up the code for this screen:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change any of the text, save the file, and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
+        <View style={styles.marginTop}>
+          <Text>Serviços</Text>
+          <View style={styles.flex}>{servicos}</View>
+          <TouchableOpacity
+            onPress={() => setIsShowingMoreServices((prev) => !prev)}
+          >
+            <Text style={styles.mostrarMais}>{servicesMessage}</Text>
           </TouchableOpacity>
         </View>
       </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-        <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>navigation/BottomTabNavigator.js</MonoText>
-        </View>
-      </View>
     </View>
-  );
-}
-
-HomeScreen.navigationOptions = {
-  header: null,
-};
-
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use useful development
-        tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
   );
 }
 
@@ -93,87 +105,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
     paddingTop: 30,
+    padding: 16,
   },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
+  content: {
+    marginTop: 16,
   },
-  welcomeImage: {
+  image: {
     width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
+    height: 100,
+    margin: 4,
   },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
+  flex: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    flexWrap: 'wrap',
   },
-  homeScreenFilename: {
-    marginVertical: 7,
+  marginTop: {
+    marginTop: 16,
   },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  mostrarMais: {
+    textAlign: 'right',
   },
 });
