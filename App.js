18998dc9832813c7a4b31c2b13924b89378bf12c
caio nakai <kaioesperto@hotmail.com>
2020-06-09 14:46:07 +0100

    [screen] added ranking screen

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
+import { Provider as PaperProvider } from "react-native-paper";
+import { Platform, StatusBar, StyleSheet, View } from "react-native";
 
-import useCachedResources from './hooks/useCachedResources';
-import LinkingConfiguration from './navigation/LinkingConfiguration';
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import HomeScreen from './screens/HomeScreen';
-import BottomTabNavigator from './navigation/BottomTabNavigator';
+import useCachedResources from "./hooks/useCachedResources";
+import LinkingConfiguration from "./navigation/LinkingConfiguration";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import BottomTabNavigator from "./navigation/BottomTabNavigator";
+import RankingScreen from "./screens/RankingScreen";
 
 const Stack = createStackNavigator();
 
@@ -22,11 +22,12 @@ export default function App(props) {
     return (
       <PaperProvider>
         <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
+          {Platform.OS === "ios" && <StatusBar barStyle="dark-content" />}
           <NavigationContainer linking={LinkingConfiguration}>
             <Stack.Navigator>
-              <Stack.Screen name="Register" component={RegisterScreen} />
               <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen name="Ranking" component={RankingScreen} />
+              <Stack.Screen name="Register" component={RegisterScreen} />
               <Stack.Screen name="Home" component={BottomTabNavigator} />
             </Stack.Navigator>
           </NavigationContainer>
@@ -39,6 +40,6 @@ export default function App(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });
