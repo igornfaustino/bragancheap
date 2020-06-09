2e4118d83e39242a45cb59397d9112ad7dc4cbb9
igornfaustino <igornfaustino@gmail.com>
2020-06-09 09:35:48 +0100

    [screen] add login screen

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 
 import useCachedResources from './hooks/useCachedResources';
-import BottomTabNavigator from './navigation/BottomTabNavigator';
 import LinkingConfiguration from './navigation/LinkingConfiguration';
+import LoginScreen from './screens/LoginScreen';
 
 const Stack = createStackNavigator();
 
@@ -16,14 +17,16 @@ export default function App(props) {
     return null;
   } else {
     return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
-        <NavigationContainer linking={LinkingConfiguration}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </View>
+      <PaperProvider>
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
+          <NavigationContainer linking={LinkingConfiguration}>
+            <Stack.Navigator>
+              <Stack.Screen name="Login" component={LoginScreen} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </View>
+      </PaperProvider>
     );
   }
 }
---
+++ screens/LoginScreen.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+  Text,
+} from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
+
+const castelo = require('../assets/images/castelo.jpeg');
+
+export default function LoginScreen({ navigation }) {
+  navigation.setOptions({ headerShown: false });
+
+  return (
+    <ImageBackground source={castelo} style={styles.container}>
+      <View style={styles.contentContainer}>
+        <Text style={styles.title}>Bem vindo de volta!</Text>
+        <TextInput style={styles.input} label="Digite o seu email" />
+        <TextInput style={styles.input} label="Digite a sua senha" />
+        <Button
+          icon="login"
+          mode="contained"
+          onPress={() => console.log('Pressed')}
+        >
+          Login
+        </Button>
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 8,
+    justifyContent: 'center',
+  },
+  input: {
+    marginBottom: 8,
+  },
+  contentContainer: {
+    padding: 16,
+    borderRadius: 10,
+    backgroundColor: '#ffffff80',
+  },
+  title: {
+    fontSize: 24,
+    alignSelf: 'center',
+    marginBottom: 16,
+  },
+});
